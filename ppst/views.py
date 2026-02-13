b2560c0ad08e8cdabf1f5621b611d5c23dbe64ae
Harrison Springer <harrisonspringer0604@gmail.com>
2026-02-12 21:41:22 -0500

    Update views.py
    Editing to implement my pages

--- ppst/views.py
+++ ppst/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Main landing page for CogniFlow platform"""
     return render(request, "ppst/index.html")
 
 
 def login(request):
+    """Login page for clinicians and administrators"""
     return render(request, "ppst/login.html")
+
+
+def patient_access(request):
+    """Patient access page where they enter their unique test code"""
+    return render(request, "ppst/patient_access.html")
