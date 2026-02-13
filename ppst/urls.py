81b6f17e4e8d70e6f0dd82a23dd7715b3af5c2e3
Harrison Springer <harrisonspringer0604@gmail.com>
2026-02-12 21:40:53 -0500

    Update urls.py
    Edited to implement my example pages

--- ppst/urls.py
+++ ppst/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
+    path("patient-access/", views.patient_access, name="patient_access"),
 ]
